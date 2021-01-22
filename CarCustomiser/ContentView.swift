3032a5acb2a6b410ff97193f7dfe5c577c26647e
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-01-22 19:17:06 +0000

    Made a "next car" button

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -11,17 +11,21 @@ import SwiftUI
 struct ContentView: View {
     //brackets as you want a new one, not the whole struct
     let starterCars = StarterCars()
-    var selectedCar: Int = 0
+    //@State says that this variable influences the view - so any time it is updated, the view will be updated
+    //worth making private because this will only be used by this view
+    @State private var selectedCar: Int = 0
+    
+    @State prviate var exhaustPackage = false
     
     //this must return something of type View
     var body: some View {
         //some View is only one view i.e. body can only return one view and Button and Text are separate views, so you need VStack 
-        VStack {
+        VStack(alignment: .leading, spacing: 20) {
             Text(starterCars.cars[selectedCar].displayStats())
-                .padding()
-            Button("Random Car", action: {
-                print("Button pressed")
+            Button("Next Car", action: {
+                selectedCar = (selectedCar + 1) % self.starterCars.cars.count
             })
+            Toggle("Exhaust Package", isOn: <#T##Binding<Bool>#>)
         }
         
     }
